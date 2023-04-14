d7c1c245724f135f2ec731d2132c871714cda92c
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-14 13:04:53 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,39 +1,40 @@
 # python3
+
 def read_input():
     pattern = ''
     text = ''
     chc = input().rstrip().lower()
-    if chc== 't':
+    if chc == 't':
         pattern = input().rstrip()
-        text = input().rstrip()
+        text =input().rstrip()
     elif chc == 'f':
         with open(input().rstrip(),'r') as F:
             pattern = F.readline().rstrip()
             text = F.readline().rstrip()
-    return pattern, text
+    return pattern,text
 def print_occurrences(output):
     print(' '.join(map(str,output)))
-def get_occurrences(pattern,text):
+def get_occurrences(pattern, text):
     p = 31
-    m =10**9+9
-    occur =[]
-    pattern_hash = 0
-    for i in range(len(pattern)):
-        pattern_hash = (pattern_hash + ord(pattern[i]) * pow(p, i, m))% m
+    m = 10**9+9
+    occur = []
+    pattern_hash= 0
     text_hash = 0
+    p_pow=1
     for i in range(len(pattern)):
-        text_hash =(text_hash + ord(text[i])*pow(p,i,m)) % m
-    if pattern_hash == text_hash and pattern == text[:len(pattern)]:
-        occur.append(0)
-    p_pow =pow(p,len(pattern),m)
-    for i in range(1,len(text)-len(pattern)+1):
-        text_hash = (text_hash-ord(text[i-1])*p_pow)% m
-        text_hash= (text_hash*p+ord(text[i+len(pattern)-1])) % m 
+        pattern_hash =(pattern_hash*p+ord(pattern[i]))% m
+        text_hash = (text_hash*p+ord(text[i])) % m
+        p_pow =(p_pow*p) % m
+    for i in range(len(text)-len(pattern)+1):
         if pattern_hash == text_hash and pattern == text[i:i+len(pattern)]:
             occur.append(i)
+        if i +len(pattern)<len(text):
+            text_hash=(text_hash-ord(text[i])*p_pow) % m
+            text_hash =(text_hash*p+ord(text[i+len(pattern)])) % m
     return occur
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
+
 
 
