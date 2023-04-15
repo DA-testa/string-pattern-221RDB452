96635958812bdf78806240b2d4d2270eaac52192
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-15 13:52:49 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -7,28 +7,29 @@ def read_input():
     text = input().strip()
     return pattern, text
 
-def hash_func(s, p, x, m):
-    h = np.zeros(m+1)
-    h[0] = 0
-    for i in range(1, m+1):
-        h[i] = (x*h[i-1] + ord(s[i-1])) % p
+def precompute_hashes(text, pattern_len, prime, multiplier):
+    hashes = np.zeros(len(text) - pattern_len + 1, dtype=int)
+    hashes[-1] = polynomial_hash(text[-pattern_len:], prime, multiplier)
+    for i in range(len(text) - pattern_len - 1, -1, -1):
+        hashes[i] = (multiplier * hashes[i + 1] + ord(text[i]) - ord(text[i + pattern_len]) * pow(multiplier, pattern_len)) % prime
+    return hashes
+
+def polynomial_hash(s, prime, multiplier):
+    h = 0
+    for c in reversed(s):
+        h = (h * multiplier + ord(c)) % prime
     return h
 
 def get_occurrences(pattern, text):
-    p = 10**9+7
-    x = np.random.randint(1, p-1)
-    m, n = len(pattern), len(text)
-    result = []
-    p_hash = hash_func(pattern, p, x, m)[-1]
-    h = hash_func(text, p, x, n)
-    for i in range(n-m+1):
-        if h[i+m]-x*h[i] == p_hash:
-            if text[i:i+m] == pattern:
-                result.append(i)
-    return result
+    prime = 10**9 + 7
+    multiplier = np.random.randint(1, prime)
+    pattern_hash = polynomial_hash(pattern, prime, multiplier)
+    hashes = precompute_hashes(text, len(pattern), prime, multiplier)
+    return [i for i in range(len(text) - len(pattern) + 1) if pattern_hash == hashes[i]]
 
 if __name__ == '__main__':
     pattern, text = read_input()
     occurrences = get_occurrences(pattern, text)
-    print(" ".join(map(str, occurrences)))
+    print(" ".join(str(i) for i in occurrences))
+
 
