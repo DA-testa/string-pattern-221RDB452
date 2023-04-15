847578d41876b2d39be988cb976472a462f86267
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-15 13:45:34 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -7,20 +7,28 @@ def read_input():
     text = input().strip()
     return pattern, text
 
-def hash_substring(pattern, text):
-    p_len = len(pattern)
-    t_len = len(text)
+def hash_func(s, p, x, m):
+    h = np.zeros(m+1)
+    h[0] = 0
+    for i in range(1, m+1):
+        h[i] = (x*h[i-1] + ord(s[i-1])) % p
+    return h
 
-    p_hash = hash(pattern)
-
-    t_hashes = np.zeros(t_len - p_len + 1, dtype=np.int64)
-    t_hashes[0] = hash(text[:p_len])
-    for i in range(1, t_len - p_len + 1):
-        t_hashes[i] = hash(text[i:i + p_len])
-
-    matches = [str(i) for i in range(t_len - p_len + 1) if t_hashes[i] == p_hash]
-    return ' '.join(matches)
+def get_occurrences(pattern, text):
+    p = 10**9+7
+    x = np.random.randint(1, p-1)
+    m, n = len(pattern), len(text)
+    result = []
+    p_hash = hash_func(pattern, p, x, m)[-1]
+    h = hash_func(text, p, x, n)
+    for i in range(n-m+1):
+        if h[i+m]-x*h[i] == p_hash:
+            if text[i:i+m] == pattern:
+                result.append(i)
+    return result
 
 if __name__ == '__main__':
     pattern, text = read_input()
-    print(hash_substring(pattern, text))
+    occurrences = get_occurrences(pattern, text)
+    print(" ".join(map(str, occurrences)))
+
