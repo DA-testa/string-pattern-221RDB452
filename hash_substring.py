d2041ddddddc67259c173f31016320bad0898e81
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-15 13:39:15 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,28 +1,26 @@
 # python3
 
-import sys
+import numpy as np
+
 def read_input():
     pattern = input().strip()
     text = input().strip()
     return pattern, text
-def get_occurrences(pattern, text):
-    occurrences = []
-    for i in range(len(text) - len(pattern) + 1):
-        if text[i:i+len(pattern)] == pattern:
-            occurrences.append(i)
-    return occurrences
-def print_occurrences(occurrences):
-    print(len(occurrences))
-    for index in occurrences:
-        print(index, end=' ')
 
+def hash_substring(pattern, text):
+    p_len = len(pattern)
+    t_len = len(text)
+
+    p_hash = hash(pattern)
+
+    t_hashes = np.zeros(t_len - p_len + 1, dtype=np.int64)
+    t_hashes[0] = hash(text[:p_len])
+    for i in range(1, t_len - p_len + 1):
+        t_hashes[i] = hash(text[i:i + p_len])
+
+    matches = [str(i) for i in range(t_len - p_len + 1) if t_hashes[i] == p_hash]
+    return ' '.join(matches)
 
 if __name__ == '__main__':
     pattern, text = read_input()
-    occurrences = get_occurrences(pattern, text)
-    print_occurrences(occurrences)
-
-
-
-
-
+    print(hash_substring(pattern, text))
