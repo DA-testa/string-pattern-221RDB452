280da0d13bebc7915455c1ebc0533e803b6193da
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-15 13:33:42 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,33 +1,27 @@
 # python3
 
+import sys
 def read_input():
-    pattern, text = input().split()
+    pattern = input().strip()
+    text = input().strip()
     return pattern, text
-
-def print_occurrences(output):
-    print(' '.join(map(str, output)))
-
 def get_occurrences(pattern, text):
-    p = 31
-    m = 10**9 + 9
-    occur = []
-    pattern_hash = 0
-    for i in range(len(pattern)):
-        pattern_hash = (pattern_hash + ord(pattern[i]) * pow(p, i, m)) % m
-    text_hash = 0
-    for i in range(len(pattern)):
-        text_hash = (text_hash + ord(text[i]) * pow(p, i, m)) % m
-    p_pow = pow(p, len(pattern), m)
+    occurrences = []
     for i in range(len(text) - len(pattern) + 1):
-        if pattern_hash == text_hash and pattern == text[i:i+len(pattern)]:
-            occur.append(i)
-        if i < len(text) - len(pattern):
-            text_hash = (text_hash - ord(text[i]) * p_pow) % m
-            text_hash = (text_hash * p + ord(text[i+len(pattern)])) % m
-    return occur
+        if text[i:i+len(pattern)] == pattern:
+            occurrences.append(i)
+    return occurrences
+def print_occurrences(occurrences):
+    print(len(occurrences))
+    for index in occurrences:
+        print(index, end=' ')
+
 
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    pattern, text = read_input()
+    occurrences = get_occurrences(pattern, text)
+    print_occurrences(occurrences)
+
 
 
 
