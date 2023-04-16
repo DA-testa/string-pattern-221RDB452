5db5074748546982eca612f094ab0500f21a65a2
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-16 23:15:03 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,47 +1,41 @@
 # python3
 
-def read_input_console():
-    pattern = input().rstrip()
-    text = input().rstrip()
-    return pattern, text
-
-def read_input_file():
-    with open(input().rstrip(),'r') as f:
-        pattern = f.readline().rstrip()
-        text = f.readline().rstrip()
-    return pattern, text
-
-def print_occurrences(output):
-    if not output:
-        print("Pattern not found")
+def read_input():
+    input_type == 'I':
+        pattern = input().strip()
+        text = input().strip()
+        return (pattern, text)
+    elif input_type == 'F':
+        with open("tests/06", "r") as file:
+            pattern = file.rreadline().strip()
+            text = file.readline().strip()
+        return (pattern, text)
     else:
-        print(' '.join(map(str,output)))
-
-def get_occurrences(pattern, text):
+        print("error")
+        return
+def print_occurrences(output):
+    print(' '.join(map(str,output)))
+def get_occurrences(pattern,text):
     p = 31
     m = 10**9+9
-    p_pow = pow(p, len(pattern), m)
-    pattern_hash = sum(ord(c) * pow(p, i, m) for i, c in enumerate(pattern)) % m
-    text_hash = sum(ord(c) * pow(p, i, m) for i, c in enumerate(text[:len(pattern)])) % m
     occur = []
+    pattern_hash = 0
+    for i in range(len(pattern)):
+        pattern_hash = (pattern_hash+ord(pattern[i])*pow(p,i,m)) % m
+    text_hash = 0
+    for i in range(len(pattern)):
+        text_hash = (text_hash+ord(text[i])*pow(p,i,m)) % m
     if pattern_hash == text_hash and pattern == text[:len(pattern)]:
         occur.append(0)
-    for i in range(1, len(text)-len(pattern)+1):
-        text_hash = (text_hash - ord(text[i-1]) * p_pow) % m
-        text_hash = (text_hash * p + ord(text[i+len(pattern)-1])) % m
+    p_pow = pow(p, len(pattern), m)
+    for i in range(1,len(text)-len(pattern)+1):
+        text_hash = (text_hash-ord(text[i-1])*p_pow) % m
+        text_hash = (text_hash*p+ord(text[i+len(pattern)-1])) % m
         if pattern_hash == text_hash and pattern == text[i:i+len(pattern)]:
             occur.append(i)
     return occur
 
 if __name__ == '__main__':
-    chc = input().rstrip().lower()
-    if chc =='I':
-        pattern, text = read_input_console()
-    elif chc=='F':
-        pattern, text = read_input_file()
-    else:
-        print("Invalid choice")
-        exit(1)
-    print_occurrences(get_occurrences(pattern, text))
+    print_occurrences(get_occurrences(*read_input()))
 
 
