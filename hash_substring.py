d36e42d330f11d82f86aa3f70c89427d97b54eb7
221RDB452 <124876380+221RDB452@users.noreply.github.com>
2023-04-16 23:51:02 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -17,22 +17,16 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str,output)))
 def get_occurrences(pattern,text):
-    p = 31
-    m = 10**7+19
+pl = len(pattern)
+    ph =sum(ors(c) for c in pattern)
+    th = sum(ord(text[i]) for i in range(pl))
+    tl= len(text)
     occur = []
-    pattern_hash = 0
-    for i in range(len(pattern)):
-        pattern_hash = (pattern_hash+ord(pattern[i])*pow(p,i,m)) % m
-    text_hash = 0
-    for i in range(len(pattern)):
-        text_hash = (text_hash+ord(text[i])*pow(p,i,m)) % m
-    if pattern_hash == text_hash and pattern == text[:len(pattern)]:
+    if ph == th and text[:pl] == pattern:
         occur.append(0)
-    p_pow = pow(p, len(pattern), m)
-    for i in range(1,len(text)-len(pattern)+1):
-        text_hash = (text_hash-ord(text[i-1])*p_pow) % m
-        text_hash = (text_hash*p+ord(text[i+len(pattern)-1])) % m
-        if pattern_hash == text_hash and pattern == text[i:i+len(pattern)]:
+    for i in range(1, tl - pl +1):
+        th = th - ord(text[i - 1])+ ord(text[i + pl - 1])
+        if ph == th and text[i:i + pl] == pattern:
             occur.append(i)
     return occur
 
